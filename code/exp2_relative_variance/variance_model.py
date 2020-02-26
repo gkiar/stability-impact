7ef388cdf27397d03d6a3b6c37dfa2627b6fed88
Greg Kiar <gkiar07@gmail.com>
2020-02-26 15:45:26 -0500

    modified model to also evaluate interaction terms

--- code/exp2_relative_variance/variance_model.py
+++ code/exp2_relative_variance/variance_model.py
@@ -17,16 +17,16 @@ df = pd.read_hdf()
 #  - mca (categorical)
 
 # Set the linear model equation
-model_equation = "graph ~ subject + session + pipeline + seed + directions"
+model_equation = "graph ~ C(subject)*C(session)*C(pipeline)*C(seed)*C(directions)"
 
 # Filter dataframe to only have a single instance per individual. Simply: pick
 # the one corresponding to MCA sim numero uno
 df_nomca = df[df["mca"] == "1"]
-model_nomca = ols(model_equation, data=df_nomca)
+model_nomca = ols(model_equation, data=df_nomca).fit()
 aovtable_nomca = sm.stats.anova_lm(model_nomca, typ=2)
 print(aovtable_nomca)
 
 # Re-perform the experiment with the MCA simulations included
-model_mca = ols(model_equation, data=df)
+model_mca = ols(model_equation, data=df).fit()
 aovtable_mca = sm.stats.anova_lm(model_mca, typ=2)
 print(aovtable_mca)
